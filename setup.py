1b66f935c8bf36e7ffc960fe77b37efd2e8b3cca
John-Mark Bell <jmb@pexip.com>
2013-05-10 11:21:25 +0100

    Imported Upstream version 0.9.11

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
 setup(
     name='django-tastypie',
-    version='0.9.10',
+    version='0.9.11',
     description='A flexible & capable API layer for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
     url='http://github.com/toastdriven/django-tastypie/',
+    long_description=open('README.rst', 'r').read(),
     packages=[
         'tastypie',
         'tastypie.utils',
+        'tastypie.management',
+        'tastypie.management.commands',
+        'tastypie.migrations',
     ],
     package_data={
         'tastypie': ['templates/tastypie/*'],
@@ -19,12 +28,10 @@ setup(
     requires=[
         'mimeparse',
         'python_dateutil(>=1.5, < 2.0)',
-        'python_digest',
     ],
     install_requires=[
         'mimeparse',
         'python_dateutil >= 1.5, < 2.0',
-        'python_digest',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
